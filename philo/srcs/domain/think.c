8c322bff5ebc75ea1de0f45a079870cc6c36918d
42Tokyonaokix <ai.business.naokix@gmail.com>
2026-05-07 14:20:32 +0900

    fix: think.c more readable

--- philo/srcs/domain/think.c
+++ philo/srcs/domain/think.c
@@ -13,7 +13,6 @@
 #include "../../includes/philo.h"
 
 static int	ms_thinking_time(t_system *system, t_philo *philo);
-static int	ms_fix(int ms_time, t_system *system);
 
 int	philo_think(t_system *system, t_philo *philo)
 {
@@ -32,30 +31,29 @@ int	philo_think(t_system *system, t_philo *philo)
 	return (flag);
 }
 
+/*  */
+
 static int	ms_thinking_time(t_system *system, t_philo *philo)
 {
 	int	num_groups;
 	int	group_id;
 	int	eat_time;
 	int	slp_time;
+	int	think_time;
 
 	eat_time = system->time_to_eat;
 	slp_time = system->time_to_sleep;
 	num_groups = (system->num_philos + 1) / 2;
 	group_id = ((philo->id - 1) / 2 + 1) % num_groups;
 	if (philo->meals_eaten == 0 && philo->id % 2 == 1)
-		return (0);
-	if (system->num_philos % 2 == 0)
-		return (ms_fix(eat_time - slp_time, system));
-	if (group_id == eat_time % num_groups)
-		return (ms_fix(eat_time * 2 - slp_time, system));
-	return (ms_fix(eat_time - slp_time, system));
-}
-
-static int	ms_fix(int ms_time, t_system *system)
-{
-	if (ms_time < (0))
-		return (0);
-	return (ms_time);
-	(void)system;
+		think_time = 0;
+	else if (system->num_philos % 2 == 0)
+		think_time = eat_time - slp_time;
+	else if (group_id == eat_time % num_groups)
+		think_time = eat_time * 2 - slp_time;
+	else
+		think_time = eat_time - slp_time;
+	if (think_time < 0)
+		think_time = 0;
+	return (think_time);
 }
